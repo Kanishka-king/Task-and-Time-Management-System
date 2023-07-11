5ad65f207db9cb27ea58c39d4ec8b6bfdb0be9a8
kanishka <94542137+Kanishka-king@users.noreply.github.com>
2023-07-11 14:46:50 +0530

    Add files via upload

---
+++ projects/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import './Navbar.css';
+import { IconContext } from 'react-icons';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+          <AiOutlineMenu onClick={showSidebar}  />
+           
+          </Link><div className='ass'>
+            <Link to="/login">
+          <button className="login-button">Login</button></Link></div>
+          <div className='siu'><Link to="/">
+          <button className="sign-button">Sign Up</button></Link></div>
+    
+        </div>
+        
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            <br/>
+            <h3 >ALL BOARDS(6)</h3>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+    
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ projects/src/login/Logout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Logout=()=>{
+    return(<div>
+        <h1>Welcome<span className="user_name">Kani</span>
+        <button className="logout_button">Logout</button>
+        </h1>
+    </div>)
+}
